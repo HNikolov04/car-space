b475201ba7e44be171ee82a6baf1bd3eab633aa3
HNikolov04 <h.nikolov2004@gmail.com>
2025-07-29 11:57:50 +0300

    Fixed not proper indentation in the code

--- server/Services/CarSpace.Services.Core/Contracts/CarShop/Requests/UpdateCarsAndSuvsListingRequest.cs
+++ server/Services/CarSpace.Services.Core/Contracts/CarShop/Requests/UpdateCarsAndSuvsListingRequest.cs
@@ -1,20 +1,20 @@
 ﻿namespace CarSpace.Services.Core.Contracts.CarShop.Requests;
 
 public sealed record UpdateCarsAndSuvsListingRequest(
-Guid Id,
-string Title,
-string Description,
-string Brand,
-string Model,
-int Year,
-int Mileage,
-int Horsepower,
-string Transmission,
-string FuelType,
-string Color,
-string EuroStandard,
-int Doors,
-decimal Price,
-string City,
-string ImageUrl
+    Guid Id,
+    string Title,
+    string Description,
+    string Brand,
+    string Model,
+    int Year,
+    int Mileage,
+    int Horsepower,
+    string Transmission,
+    string FuelType,
+    string Color,
+    string EuroStandard,
+    int Doors,
+    decimal Price,
+    string City,
+    string ImageUrl
 );
