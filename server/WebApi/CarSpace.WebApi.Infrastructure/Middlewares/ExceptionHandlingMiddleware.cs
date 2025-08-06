3ebd2a71fe3a4f00bbe3d4e4ced0e3ed905b1397
HNikolov04 <h.nikolov2004@gmail.com>
2025-08-06 23:41:34 +0300

    Add custom middleware

---
+++ server/WebApi/CarSpace.WebApi.Infrastructure/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using CarSpace.Services.Core.Exceptions;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Text.Json;
+
+namespace CarSpace.WebApi.Middleware;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = ex switch
+            {
+                NotFoundException => (int)HttpStatusCode.NotFound,
+                UnauthorizedAccessAppException => (int)HttpStatusCode.Forbidden,
+                ValidationAppException => (int)HttpStatusCode.BadRequest,
+                _ => (int)HttpStatusCode.InternalServerError
+            };
+
+            var response = new
+            {
+                error = ex.Message,
+                status = context.Response.StatusCode
+            };
+
+            var json = JsonSerializer.Serialize(response);
+
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
