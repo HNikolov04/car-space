50042273576ab678758543d8d3fd56f0536ec372
HNikolov04 <h.nikolov2004@gmail.com>
2025-08-06 23:36:30 +0300

    Add About and Contact domain models

---
+++ server/Data/CarSpace.Data.Models/Entities/About/AboutUs.cs
@@ -0,0 +1,9 @@
+﻿namespace CarSpace.Data.Models.Entities.About;
+
+public class AboutUs
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Title { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+}
---
+++ server/Data/CarSpace.Data.Models/Entities/Contact/ContactUs.cs
@@ -0,0 +1,12 @@
+﻿namespace CarSpace.Data.Models.Entities.Contact;
+
+public class ContactUs
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Title { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+}
+
